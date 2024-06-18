592f6535e6981e8fcd2e269858e978a6cd763e33
Alecaring <Private_a.a.c@icloud.com>
2024-06-19 01:12:51 +0200

    add: edit

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -2,8 +2,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>{{ $comic->titolo }}</h1>
+    <div class="container p-0 d-flex flex-column align-items-center justify-content-center">
+        <a href="{{ route('comic.edit', $comic->id) }}" type="submit" class="btn btn-primary my-4">Modifica</a>
+        <img src="{{ $comic->image }}" alt="">
+        <h1>{{ $comic->nome }}</h1>
         <p>{{ $comic->descrizione }}</p>
+        <p>{{ $comic->price }}</p>
+        <p>{{ $comic->adress }}</p>
+        <p>{{ $comic->bedroom }}</p>
+        <p>{{ $comic->bathroom }}</p>
+        <p>{{ $comic->square_meters }}</p>
+        <p>{{ $comic->property_type }}</p>
+        <p>{{ $comic->status }}</p>
+        <p>{{ $comic->built_year }}</p>
     </div>
 @endsection
